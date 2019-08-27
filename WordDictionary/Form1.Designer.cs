1792c43057dbbc767f57b49ffc7961c734a7e0bf
ZayVK <jay.v.kamble@gmail.com>
2019-08-27 17:39:38 +0530

    Word Dictionary problem commit

---
+++ WordDictionary/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WordDictionary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.searchTextbox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.browseTextbox = new System.Windows.Forms.TextBox();
+            this.uploadButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(221, 256);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Search Keyword";
+            // 
+            // searchTextbox
+            // 
+            this.searchTextbox.Location = new System.Drawing.Point(491, 253);
+            this.searchTextbox.Name = "searchTextbox";
+            this.searchTextbox.Size = new System.Drawing.Size(178, 22);
+            this.searchTextbox.TabIndex = 1;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(334, 310);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(151, 34);
+            this.searchButton.TabIndex = 2;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "WordDisctionary";
+            this.openFileDialog1.Title = "Select a file for Word Disctionary";
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(239, 55);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 3;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // browseTextbox
+            // 
+            this.browseTextbox.Location = new System.Drawing.Point(491, 56);
+            this.browseTextbox.Name = "browseTextbox";
+            this.browseTextbox.Size = new System.Drawing.Size(178, 22);
+            this.browseTextbox.TabIndex = 4;
+            // 
+            // uploadButton
+            // 
+            this.uploadButton.Location = new System.Drawing.Point(239, 110);
+            this.uploadButton.Name = "uploadButton";
+            this.uploadButton.Size = new System.Drawing.Size(151, 32);
+            this.uploadButton.TabIndex = 5;
+            this.uploadButton.Text = "Upload";
+            this.uploadButton.UseVisualStyleBackColor = true;
+            this.uploadButton.Click += new System.EventHandler(this.UploadButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(488, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(221, 382);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.uploadButton);
+            this.Controls.Add(this.browseTextbox);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchTextbox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox searchTextbox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox browseTextbox;
+        private System.Windows.Forms.Button uploadButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WordDictionary/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WordDictionary
+{
+    public partial class Form1 : Form
+    {
+        #region Constants
+        public static string rootPath = Application.StartupPath;
+        public static string destination = rootPath + @"\TempLocation\WordDictionary.txt";
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = @"D:\",
+                Title = "Select the file for word dictionary",
+                CheckFileExists = true,
+                CheckPathExists = true,
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                browseTextbox.Text = openFileDialog.SafeFileName;
+                label2.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void UploadButton_Click(object sender, EventArgs e)
+        {
+            string fileToUpload;
+            FileInfo fileInfo = new FileInfo(label2.Text);
+            long fileSize = fileInfo.Length / (1024 * 1024);
+            if (fileSize < 200)
+            {
+                using (StreamReader reader = new StreamReader(label2.Text))
+                {
+                    fileToUpload = reader.ReadToEnd();
+                }
+
+                if (!string.IsNullOrEmpty(fileToUpload))
+                {
+                    using (StreamWriter writer = new StreamWriter(destination, true))
+                    {
+                        writer.Write(" " + fileToUpload);
+                        label2.Text = "Upload Successful";
+                    }
+                }
+            }
+            else
+                label2.Text = "File Size Exceeds. Must be less than 200MB";
+            browseTextbox.Text = "";
+
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            FileStream fileStream = new FileStream(destination, FileMode.Open, FileAccess.Read);
+
+            string fileContent;
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                fileContent = reader.ReadToEnd();
+            }
+
+            string[] contentArr = fileContent.Split(' ');
+            string searchKeyword = searchTextbox.Text;
+
+            var searchWord = Array.FindAll(contentArr, str => str.ToLower().Equals(searchKeyword.ToLower()));
+            if (searchWord.Length > 0)
+            {
+                label3.Text = "Search successful. The word is " + searchWord.Length + " times in the dictionary";
+            }
+            else
+                label3.Text = "Word is not present in the dictionary";
+
+            searchTextbox.Text = "";
+        }
+    }
+}
